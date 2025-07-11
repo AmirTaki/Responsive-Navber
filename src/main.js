c67cfa63eb561b6b85348bb7d99a035a9bf8c6a3
amir <amirtaki623@gmail.com>
2025-07-11 22:01:41 +0330

    querySelector js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,3 @@
-s
+const toggleBtn = document.querySelector(".toggle_btn")
+const toggleBtnIcon = document.querySelector(".toggle_btn i")
+const dropDwonMenu = document.querySelector(".dropdown_menu")
