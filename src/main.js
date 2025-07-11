ca9eb0890beb8dab80c8bd9ce9339c975e12a58e
amir <amirtaki623@gmail.com>
2025-07-11 22:04:32 +0330

    add icons class js at project

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 const toggleBtn = document.querySelector(".toggle_btn")
 const toggleBtnIcon = document.querySelector(".toggle_btn i")
 const dropDwonMenu = document.querySelector(".dropdown_menu")
+
+toggleBtn.addEventListener('click', ()=>{
+    dropDwonMenu.classList.toggle('open')
+    const isOpen = dropDwonMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"
+})
